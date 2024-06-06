67c4c085ed60d43288b6f05e5b17dc0fd3ecaf48
chengwu <2477376240@qq.com>
2024-06-06 18:24:51 +0800

    配置全局跨域

---
+++ gateway/src/main/java/com/chengwu/gateway/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.chengwu.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+import java.util.Arrays;
+
+/**
+ * 全局跨域配置
+ */
+// 处理跨域
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
+        // 可以改为线上真实域名、本地域名
+//        config.setAllowedOriginPatterns("localhost:8080");
+        config.setAllowedOriginPatterns(Arrays.asList("*"));
+        config.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+}
