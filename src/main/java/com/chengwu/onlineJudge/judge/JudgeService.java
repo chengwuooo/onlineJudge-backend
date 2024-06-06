eb5dc564402118c448f8de28c03abec3f965e2dd
chengwu <2477376240@qq.com>
2024-06-06 20:35:37 +0800

    删除不必要的文件

--- src/main/java/com/chengwu/onlineJudge/judge/JudgeService.java
+++
@@ -1,8 +0,0 @@
-package com.chengwu.onlineJudge.judge;
-
-import com.chengwu.onlineJudge.model.entity.QuestionSubmit;
-
-public interface JudgeService {
-    public QuestionSubmit doJudge(long questionId);
-
-}
--- src/main/java/com/chengwu/onlineJudge/judge/strategy/JudgeStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.chengwu.onlineJudge.judge.strategy;
-
-import com.chengwu.onlineJudge.judge.codeSandbox.model.JudgeInfo;
-
-public interface JudgeStrategy {
-    JudgeInfo doJudge(JudgeContext judgeContext);
-}
